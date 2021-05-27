5faf45c3a9347046acb702fdd1e8685a7c0804d1
54696d654a6f6c74 <eniz.h12@gmail.com>
2021-05-27 14:56:56 +0300

    Settings cleanup

--- Settings.cs
+++ Settings.cs
@@ -16,16 +16,10 @@ namespace RadioTools
         public class Data
         {
             public ushort maxTasksPerThread {get; set;} = 5;
-            public string connectionString {get; set;} = "mgradio_client";
-            public ushort communicationByteSize {get; set;} = 100;
             public int connectionPort {get; set;} = 1999;
-            public string URL {get; set;} = "";
-            public string[] setURLcommand {get; set;} = {"SETURL"};
             public int timeout {get; set;} = 200;
             public string subnet {get; set;} = "192.168.0.";
             public ushort range {get; set;} = 255;
-            public ushort volume {get; set;} = 90;
-            public string setVolumeCommand {get; set;} = "SETVOLUME";
             public ushort cmdNameSize {get; set;} = 32;
             public ushort responseSize {get; set;} = 256;
             public string newCMDRequest {get; set;} = "n";
